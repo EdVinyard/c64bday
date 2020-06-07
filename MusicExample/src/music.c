bd05d258ecf85df8f6582b2883a91f3bf8d10298
Edward S. Vinyard <ed@tzahk.com>
2020-06-07 11:38:48 -0500

    easier to experiment with music voices

--- MusicExample/src/music.c
+++ MusicExample/src/music.c
@@ -17,6 +17,9 @@ __asm__("sta $d020")
 #define SAWTOOTH (32)
 #define PULSE (64)
 
+const uchar voice1 = PULSE;
+const uchar voice2 = TRIANGLE;
+
 const uchar single_voice_data[] = {
     25,177,250,
     28,214,250,
@@ -47,7 +50,6 @@ void single_voice_example() {
     uchar lf;
     uchar dr;
     uchar t;
-    const uchar voice = PULSE;
     
     SID_REGISTERS[5] = 9;
     SID_REGISTERS[6] = 0;
@@ -62,7 +64,7 @@ void single_voice_example() {
 
         SID_REGISTERS[1] = hf;
         SID_REGISTERS[0] = lf;
-        SID_REGISTERS[4] = voice | 1;
+        SID_REGISTERS[4] = voice1 | 1;
 
         // wait with sound playing
         for (; dr > 0; dr -= 10) {
@@ -73,7 +75,7 @@ void single_voice_example() {
             __asm__("bne %g", rasterwait);
         }
 
-        SID_REGISTERS[4] = voice;
+        SID_REGISTERS[4] = voice1;
 
         // wait in silence
         for (dr = 50; dr > 0; dr -= 10) {
@@ -105,19 +107,24 @@ void multiple_voice_example() {
     
     SID_REGISTERS[5] = 9; // voice 1 attack/decay
     SID_REGISTERS[6] = 0; // voice 1 sustain/release
+    SID_REGISTERS[2] = 0; // voice 1 pulse width low byte
+    SID_REGISTERS[3] = 8; // voice 1 pulse width high byte    
+
     SID_REGISTERS[5+7] = 9; // voice 2 attack/decay
     SID_REGISTERS[6+7] = 0; // voice 2 sustain/release
 
     SID_REGISTERS[24] = 15; // set max volume
 
     for (i = 0; i < sizeof(single_voice_data); i += 3) {
+        BORDER_CHANGE;
+
         hf = single_voice_data[i+0];
         lf = single_voice_data[i+1];
         dr = single_voice_data[i+2];
 
         SID_REGISTERS[1] = hf; // voice 1 frequency high byte
         SID_REGISTERS[0] = lf; // voice 1 frequency low byte
-        SID_REGISTERS[4] = 33; // voice 1 sawtooth (32) + attack initiated (1)
+        SID_REGISTERS[4] = voice1 | 1;
 
         // up a perfect fifth
         lf_up_a_fifth = lf + (lf >> 1);
@@ -128,7 +135,9 @@ void multiple_voice_example() {
 
         SID_REGISTERS[1+7] = hf_up_a_fifth; // voice 2 freq high byte
         SID_REGISTERS[0+7] = lf_up_a_fifth; // voice 2 freq low byte
-        SID_REGISTERS[4+7] = 33; // voice 2 sawtooth (32) + attack (1)
+        SID_REGISTERS[4+7] = voice2 | 1; // voice 2 + attack (1)
+
+        BORDER_RESET;
 
         // wait with sound playing
         for (; dr > 0; dr -= 10) {
@@ -139,8 +148,8 @@ void multiple_voice_example() {
             __asm__("bne %g", rasterwait);
         }
 
-        SID_REGISTERS[4] = 32; // voice 1 decay initiated
-        SID_REGISTERS[4+7] = 32; // voice 2 decay initiated
+        SID_REGISTERS[4] = voice1; // voice 1 decay initiated
+        SID_REGISTERS[4+7] = voice2; // voice 2 decay initiated
 
         // wait in silence
         for (dr = 50; dr > 0; dr -= 10) {
@@ -156,6 +165,6 @@ void multiple_voice_example() {
 void main(void)
 {
     clear_sid_registers();
-    while (1) single_voice_example();
-    //while (1) multiple_voice_example();
+    // while (1) single_voice_example();
+    while (1) multiple_voice_example();
 }
